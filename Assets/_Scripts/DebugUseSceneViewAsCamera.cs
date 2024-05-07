7616ea4884a7a5e247b28cf6b0dfdc369c7c3e75
Ryan Ernest <ryan@rivergroup.com>
2024-05-07 19:44:00 -0400

    updateOutOfPlayMode now properly works

--- Assets/_Scripts/DebugUseSceneViewAsCamera.cs
+++ Assets/_Scripts/DebugUseSceneViewAsCamera.cs
@@ -1,17 +1,39 @@
 ﻿using UnityEngine;
 #if UNITY_EDITOR
-using UnityEditor;
+    using UnityEditor;
 #endif
 
 [ExecuteInEditMode]
 public class DebugUseSceneViewAsCamera : MonoBehaviour {
     public bool updateOutOfPlayMode = false;
 
+    void OnEnable() {
+    #if UNITY_EDITOR
+        EditorApplication.update += EditorUpdate;
+    #endif
+    }
+
+    void OnDisable() {
+    #if UNITY_EDITOR
+        EditorApplication.update -= EditorUpdate;
+    #endif
+    }
+
+    void EditorUpdate() {
+        UpdateCameraPosition();
+    }
+
     private void Update() {
-        if (!Application.isEditor) return;
+        if (Application.isPlaying) {
+            UpdateCameraPosition();
+        }
+    }
+
+    private void UpdateCameraPosition() {
+        if (!Application.isEditor || SceneView.lastActiveSceneView == null) return;
 
         if ((!updateOutOfPlayMode || Application.isPlaying) && !Application.isPlaying) return;
-        
+
         transform.position = SceneView.lastActiveSceneView.camera.transform.position;
         transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
     }
