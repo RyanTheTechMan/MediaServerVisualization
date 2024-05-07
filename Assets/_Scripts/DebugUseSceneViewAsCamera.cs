6f95fb4dde07a8db51684a37d105ea4457106111
Ryan Ernest <ryan@rivergroup.com>
2024-05-07 19:35:58 -0400

    Added DebugUseSceneViewAsCamera

---
+++ Assets/_Scripts/DebugUseSceneViewAsCamera.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[ExecuteInEditMode]
+public class DebugUseSceneViewAsCamera : MonoBehaviour {
+    public bool updateOutOfPlayMode = false;
+
+    private void Update() {
+        if (!Application.isEditor) return;
+
+        if ((!updateOutOfPlayMode || Application.isPlaying) && !Application.isPlaying) return;
+        
+        transform.position = SceneView.lastActiveSceneView.camera.transform.position;
+        transform.rotation = SceneView.lastActiveSceneView.camera.transform.rotation;
+    }
+}
