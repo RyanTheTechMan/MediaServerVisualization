04104f2061134b4be85b169765c904de54366f4b
NagrandHero <xander327.ag@gmail.com>
2024-05-07 19:06:54 -0600

    Added Library Style
    WIP unoptimized

--- Assets/_Scripts/SpawnStyles/LibraryStyle.cs
+++ Assets/_Scripts/SpawnStyles/LibraryStyle.cs
@@ -1,9 +1,40 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LibraryStyle : SpawnStyle {
     public override IEnumerator Create(DisplayType displayType, Vector3 position, List<MediaData> mediaData) {
-        throw new System.NotImplementedException();
+        Quaternion rotation = new Quaternion(0, 0, 0,0);
+        Vector3 xOffset = new Vector3(0, 0, 0);
+        List<DisplayType> listOfObjects = new List<DisplayType>();
+        foreach (MediaData mediaItem in mediaData) {
+            xOffset.x = xOffset.x + 1;
+            DisplayType newObject = Instantiate(displayType.gameObject).GetComponent<DisplayType>();
+            newObject.transform.position = position + xOffset;
+            newObject.transform.rotation = rotation;
+            newObject.mediaData = mediaItem;
+            listOfObjects.Add(newObject);
+        }
+
+        GameManager.instance.StartCoroutine(CamCheck(listOfObjects));
+        yield return null;
+    }
+
+    public IEnumerator CamCheck(List<DisplayType> listOfObjects) {
+        Vector3 objectPos;
+        while (true) {
+            Transform camTransform = Camera.main.transform;
+            Vector3 camPos = camTransform.position;
+            foreach (var obj in listOfObjects) {
+                objectPos = obj.transform.position;
+                if (Vector3.Distance(objectPos, camPos) >= 10) {
+                    obj.gameObject.SetActive(false);
+                }
+                else {
+                    obj.gameObject.SetActive(true);
+                }
+            }
+            yield return null;
+        }
     }
 }
