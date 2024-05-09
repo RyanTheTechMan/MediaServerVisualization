69557df3db22bd0b23ecdc30cb06c5df1e86ed15
Ryan Ernest <ryan@rivergroup.com>
2024-05-09 03:07:21 -0400

    Added Plex and Jellyfin logos

--- Assets/_Scripts/MediaSources/Jellyfin/JellyfinSource.cs
+++ Assets/_Scripts/MediaSources/Jellyfin/JellyfinSource.cs
@@ -1,3 +1,4 @@
 public class JellyfinSource : MediaSource {
-    
+    // https://jellyfin.org/docs/general/contributing/branding
+    // https://github.com/jellyfin/jellyfin-ux/tree/master/branding/SVG (banner-light / banner-dark & icon-solid-white)
 }
