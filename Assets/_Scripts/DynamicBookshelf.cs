adfe20ed1211b96d44999685449f39b4c480bdb7
Ryan Ernest <ryan@rivergroup.com>
2024-05-10 23:54:38 -0400

    Can now preview Bookshelves in the Editor

--- Assets/_Scripts/DynamicBookshelf.cs
+++ Assets/_Scripts/DynamicBookshelf.cs
@@ -1,43 +1,61 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer), typeof(MeshCollider))]
 public class DynamicBookshelf : MonoBehaviour {
-    [Range(1, 30)] public int numberOfRows = 5;
-    [Range(1, 30)] public int numberOfColumns = 3;
-    
+    [Range(0, 30)] public int numberOfRows = 5;
+    [Range(0, 30)] public int numberOfColumns = 3;
+
     [Range(0.01f, 30)] public float shelfWidth = 8f;
     [Range(0.01f, 30)] public float shelfHeight = 1f;
     [Range(0.01f, 10)] public float shelfDepth = 0.5f;
-    
+
     [Range(0.01f, 1f)] public float shelfWoodThickness = 0.1f; // Thickness of the wood used for shelves
     [Range(0.01f, 1f)] public float sideWoodThickness = 0.1f; // Thickness of the wood used for sides
-    [Range(0.01f, 1f)] public float columnWoodThickness = 0.1f; // Thickness of the wood used for vertical columns
     [Range(0.01f, 1f)] public float backWoodThickness = 0.1f; // Thickness of the wood used for the back
-    
+    [Range(0.01f, 1f)] public float columnWoodThickness = 0.1f; // Thickness of the wood used for vertical columns
+    [Range(0.01f, 1f)] public float columnDepth = 1f; // Percentage of the shelf depth that the columns should occupy
+
     public bool hasBack = true;
     public bool hasSides = true;
-    
-    [Range(0, 1)] public float backPosition = 0f; // Position of the back from 0 (back) to 1 (front)
+
+    [Range(0f, 1f)] public float backPosition = 0f; // Position of the back from 0 (back) to 1 (front)
+    [Range(0f, 1f)] public float columnPosition = 0f; // Position of the column from 0 (back) to 1 (front)
 
     private Mesh mesh;
+
     private List<Vector3> vertices;
     private List<int> triangles;
     private List<Vector2> uvs;
 
     private MeshFilter meshFilter;
+    private MeshCollider meshCollider;
 
-    private void Start() {
-        meshFilter = GetComponent<MeshFilter>();
-        GenerateBookshelf();
+    public bool updateBookshelf; // Whether we need to update the bookshelf or not
+
+    void OnValidate() {
+        if (!Application.isPlaying) updateBookshelf = true;
     }
 
-    void Update() {
+    void PrepareMesh() {
+        if (mesh == null) mesh = new Mesh();
+        if (meshCollider == null) meshCollider = GetComponent<MeshCollider>();
+        if (meshFilter == null) meshFilter = GetComponent<MeshFilter>();
+        
+        mesh.Clear();
+        
+        meshCollider.sharedMesh = mesh;
+        meshFilter.mesh = mesh;
+    }
+
+    void Awake() {
         GenerateBookshelf();
     }
 
     void GenerateBookshelf() {
-        mesh = new Mesh();
+        PrepareMesh();
+
         vertices = new List<Vector3>();
         triangles = new List<int>();
         uvs = new List<Vector2>();
@@ -48,52 +66,44 @@ public class DynamicBookshelf : MonoBehaviour {
             currentHeight += shelfHeight + shelfWoodThickness;
         }
 
-        if (numberOfColumns > 0) {
-            for (int col = 0; col < numberOfColumns; col++) {
-                AddVerticalSeparator(col);
-            }
-        }
+        for (int col = 0; col < numberOfColumns; col++) AddVerticalSeparator(col);
 
-        if (hasBack) {
-            AddBack(currentHeight);
-        }
+        if (hasBack) AddBack(currentHeight);
 
-        if (hasSides) {
-            AddSides(currentHeight);
-        }
-        
+        if (hasSides) AddSides(currentHeight);
+
         AddTopCover(currentHeight);
 
         mesh.vertices = vertices.ToArray();
         mesh.triangles = triangles.ToArray();
         mesh.uv = uvs.ToArray();
         mesh.RecalculateNormals();
-
-        meshFilter.mesh = mesh;
     }
-    
+
+    // TODO: Needs to account for back position
     void AddTopCover(float totalHeight) {
+        // to fix top to account for back position, we need to follow the depth of the back based on the back position. That means we also have to change the top position based on the back position and the back thickness
         Vector3 topBaseCorner = new Vector3((hasSides ? -sideWoodThickness : 0), totalHeight, 0);
-        AddBox(topBaseCorner, shelfWidth - (hasSides ? -sideWoodThickness*2 : 0), shelfWoodThickness, shelfDepth + (hasBack ? backWoodThickness : 0));
+        AddBox(topBaseCorner, shelfWidth - (hasSides ? -sideWoodThickness * 2 : 0), shelfWoodThickness, shelfDepth + (hasBack ? backWoodThickness : 0));
     }
 
-    // has small issue, height of each row is not correct. Also, maybe add in a option to choose how far from the back should the columns be (depth)
     void AddVerticalSeparator(int column) {
         float columnSpacing = shelfWidth / (numberOfColumns + 1);
-        
-        for (int row = 0; row < numberOfRows; row++) {
-            float yPos = row * (shelfHeight + shelfWoodThickness);
-            float xPos = (column + 1) * columnSpacing - columnWoodThickness / 2;
-            Vector3 baseCorner = new Vector3(xPos, yPos, 0);
 
-            AddBox(baseCorner, columnWoodThickness, shelfHeight + shelfWoodThickness, shelfDepth);
+        for (int row = 0; row < numberOfRows; row++) {
+            float yPos = row * shelfHeight + ((row + 1) * shelfWoodThickness);
+            float xPos = (column + 1) * columnSpacing - columnWoodThickness / 2;
+            float zPos = -shelfDepth + (columnPosition * (shelfDepth - shelfDepth * columnDepth)) + (shelfDepth * columnDepth);
+
+            Vector3 baseCorner = new Vector3(xPos, yPos, zPos);
+            AddBox(baseCorner, columnWoodThickness, shelfHeight, shelfDepth * columnDepth);
         }
     }
 
     void AddBack(float totalHeight) {
         float backZPos = -shelfDepth + (backPosition * (shelfDepth + backWoodThickness));
-        Vector3 baseCorner = new Vector3((hasSides ? - sideWoodThickness : 0), 0, backZPos);
-        AddBox(baseCorner, shelfWidth + (hasSides ? sideWoodThickness*2 : 0), totalHeight, backWoodThickness);
+        Vector3 baseCorner = new Vector3((hasSides ? -sideWoodThickness : 0), 0, backZPos);
+        AddBox(baseCorner, shelfWidth + (hasSides ? sideWoodThickness * 2 : 0), totalHeight, backWoodThickness);
     }
 
     void AddSides(float totalHeight) {
@@ -111,7 +121,7 @@ public class DynamicBookshelf : MonoBehaviour {
         AddBox(baseCorner, shelfWidth, shelfWoodThickness, shelfDepth);
     }
 
-    
+
     void AddBox(Vector3 baseCorner, float width, float height, float depth) {
         Vector3 bottomLeftFront = baseCorner;
         Vector3 bottomRightFront = baseCorner + new Vector3(width, 0, 0);
@@ -125,14 +135,14 @@ public class DynamicBookshelf : MonoBehaviour {
 
         AddQuad(bottomLeftFront, bottomRightFront, topLeftFront, topRightFront); // Front face
         AddQuad(bottomRightBack, bottomLeftBack, topRightBack, topLeftBack); // Back face
-        
+
         AddQuad(topLeftFront, topRightFront, topLeftBack, topRightBack); // Top face
         AddQuad(bottomLeftBack, bottomRightBack, bottomLeftFront, bottomRightFront); // Bottom face
-        
+
         AddQuad(bottomRightFront, bottomRightBack, topRightFront, topRightBack); // Left face
         AddQuad(bottomLeftBack, bottomLeftFront, topLeftBack, topLeftFront); // Right face
     }
-    
+
     void AddQuad(Vector3 bl, Vector3 br, Vector3 tl, Vector3 tr) {
         int startVertex = vertices.Count;
         vertices.Add(bl);
@@ -151,5 +161,10 @@ public class DynamicBookshelf : MonoBehaviour {
         uvs.Add(new Vector2(1, 0));
         uvs.Add(new Vector2(1, 1));
         uvs.Add(new Vector2(0, 1));
+    }
+    
+    public void ForceUpdate() {
+        mesh = new Mesh();
+        GenerateBookshelf();
     }
 }
---
+++ Assets/_Scripts/Editor/DynamicBookshelfEditor.cs
@@ -0,0 +1,11 @@
+﻿using UnityEditor;
+
+[CustomEditor(typeof(DynamicBookshelf)), CanEditMultipleObjects]
+public class DynamicBookshelfEditor : Editor {
+    public void OnSceneGUI() {
+        DynamicBookshelf bookshelf = (DynamicBookshelf)target;
+        if (!bookshelf.updateBookshelf) return;
+        bookshelf.ForceUpdate();
+        bookshelf.updateBookshelf = false;
+    }
+}
